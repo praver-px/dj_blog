2f9271131bd58af27fa9fae25123ef7fb6feb1d0
praver <praverpx@gmail.com>
2024-12-20 15:14:02 +0800

    注册功能实现

--- myauth/views.py
+++ myauth/views.py
@@ -3,9 +3,15 @@ import string
 
 from django.core.mail import send_mail
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.decorators.http import require_http_methods
 
+from myauth.forms import RegisterForm
 from myauth.models import CaptchaModel
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 # Create your views here.
@@ -15,8 +21,22 @@ def login(request):
     return render(request, 'login.html')
 
 
+@require_http_methods(['GET', 'POST'])
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            User.objects.create_user(email=email, username=username, password=password)
+            return redirect(reverse('myauth:login'))
+        else:
+            print(form.errors)
+            return redirect(reverse('myauth:register'))
+            # return render(request, 'register.html', context={'form': form})
 
 
 def send_email_captcha(request):
